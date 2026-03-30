d3f8bb0ba3fe85703862a576cf96d0b4402bbbc0
Kieran Rafferty <mrkrafferty@gmail.com>
2026-03-30 19:25:44 +0100

    Tidy up

--- media/lua/client/MapShare.lua
+++ media/lua/client/MapShare.lua
@@ -1,37 +1,37 @@
 MapShare = {}
 
-function MapShare:getSymbolsAPI() 
+function MapShare:getSymbolsAPI()
   return ISWorldMap_instance.javaObject:getAPIv1():getSymbolsAPI()
 end
 
 function MapShare:getCurrentMapSymbols()
-    local payload = {}
-    local symAPI = self:getSymbolsAPI()
-    local cnt = symAPI:getSymbolCount()
-    for i = 0, cnt - 1 do
-        local sym = symAPI:getSymbolByIndex(i)
-        if sym:isVisible() then
-            local s = {}
-            s.x = sym:getWorldX()
-            s.y = sym:getWorldY()
-            s.r = sym:getRed()
-            s.g = sym:getGreen()
-            s.b = sym:getBlue()
-            s.a = sym:getAlpha()
-            if sym:isTexture() then
-                s.type = "texture"
-                s.texture = sym:getSymbolID()
-            elseif sym:isText() then
-                s.type = "text"
-                s.text = sym:getTranslatedText() or sym:getUntranslatedText()
-            else
-                error("unknown symbol type at index " .. i)
-            end
+  local payload = {}
+  local symAPI = self:getSymbolsAPI()
+  local cnt = symAPI:getSymbolCount()
+  for i = 0, cnt - 1 do
+    local sym = symAPI:getSymbolByIndex(i)
+    if sym:isVisible() then
+      local s = {}
+      s.x = sym:getWorldX()
+      s.y = sym:getWorldY()
+      s.r = sym:getRed()
+      s.g = sym:getGreen()
+      s.b = sym:getBlue()
+      s.a = sym:getAlpha()
+      if sym:isTexture() then
+        s.type = "texture"
+        s.texture = sym:getSymbolID()
+      elseif sym:isText() then
+        s.type = "text"
+        s.text = sym:getTranslatedText() or sym:getUntranslatedText()
+      else
+        error("unknown symbol type at index " .. i)
+      end
 
-            table.insert(payload, s)
-        end
+      table.insert(payload, s)
     end
-    return payload
+  end
+  return payload
 end
 
 function MapShare:symbolExists(symbol)
@@ -44,10 +44,11 @@ function MapShare:symbolExists(symbol)
     local yMatch = existingSymbol:getWorldY() == symbol.y
     local textureMatch = false
 
-    if existingSymbol:isTexture() and symbol.type == "texture" then 
-      textureMatch = existingSymbol:getSymbolID() == symbol.texture 
+    if existingSymbol:isTexture() and symbol.type == "texture" then
+      textureMatch = existingSymbol:getSymbolID() == symbol.texture
     elseif existingSymbol:isText() and symbol.type == "text" then
-      textureMatch = existingSymbol:getTranslatedText() == symbol.text or existingSymbol:getUntranslatedText() == symbol.text
+      textureMatch = existingSymbol:getTranslatedText() == symbol.text or
+          existingSymbol:getUntranslatedText() == symbol.text
     else
       textureMatch = true
     end
@@ -58,31 +59,31 @@ function MapShare:symbolExists(symbol)
 end
 
 function MapShare:injectSymbolsFromTable(data)
-    local symbolApi = self:getSymbolsAPI()
-    for _, s in ipairs(data) do
-      if not self:symbolExists(s) then
-          if s.type == "texture" then
-              local sym = symbolApi:addTexture(s.texture, s.x, s.y)
-              sym:setRGBA(s.r, s.g, s.b, s.a)
-              sym:setAnchor(0.5, 0.5)
-              sym:setScale(ISMap.SCALE)
-          elseif s.type == "text" then
-              local sym = symbolApi:addTranslatedText(s.text, UIFont.Handwritten, s.x, s.y)
-              sym:setRGBA(s.r, s.g, s.b, s.a)
-              sym:setAnchor(0.0, 0.0)
-              sym:setScale(ISMap.SCALE)
-          else
-              error("unknown type found in payload " .. (s.type or "nil"))
-          end
+  local symbolApi = self:getSymbolsAPI()
+  for _, s in ipairs(data) do
+    if not self:symbolExists(s) then
+      if s.type == "texture" then
+        local sym = symbolApi:addTexture(s.texture, s.x, s.y)
+        sym:setRGBA(s.r, s.g, s.b, s.a)
+        sym:setAnchor(0.5, 0.5)
+        sym:setScale(ISMap.SCALE)
+      elseif s.type == "text" then
+        local sym = symbolApi:addTranslatedText(s.text, UIFont.Handwritten, s.x, s.y)
+        sym:setRGBA(s.r, s.g, s.b, s.a)
+        sym:setAnchor(0.0, 0.0)
+        sym:setScale(ISMap.SCALE)
+      else
+        error("unknown type found in payload " .. (s.type or "nil"))
+      end
 
-          if s.visited then
-              local offset = 5
-              WorldMapVisited.getInstance():setKnownInSquares(
-                      s.x - offset, s.y - offset, s.x + offset, s.y + offset
-              )
-          end
+      if s.visited then
+        local offset = 5
+        WorldMapVisited.getInstance():setKnownInSquares(
+          s.x - offset, s.y - offset, s.x + offset, s.y + offset
+        )
       end
     end
+  end
 end
 
 function MapShare:onShareMap(player, otherPlayer)
@@ -94,47 +95,46 @@ function MapShare:onShareMap(player, otherPlayer)
   ModData.transmit(key)
 end
 
-local function onReceiveGlobalModData(module, packet)
-    local key = "MapShare_" .. getPlayer():getDisplayName()
-    local mapData = ModData.get(key)
-    if mapData then
-      MapShare:injectSymbolsFromTable(mapData)
-      local emptyPayload = {}
-      ModData.add(key, emptyPayload)
-      ModData.transmit(key)
-    end
+function MapShare:onReceiveGlobalModData(module, packet)
+  local key = "MapShare_" .. getPlayer():getDisplayName()
+  local mapData = ModData.get(key)
+  if mapData then
+    MapShare:injectSymbolsFromTable(mapData)
+    local emptyPayload = {}
+    ModData.add(key, emptyPayload)
+    ModData.transmit(key)
+  end
 end
 
-Events.OnReceiveGlobalModData.Add(onReceiveGlobalModData)
-
-function shareMapButton(player, context, worldobjects, test)
-
+local function shareMapButton(player, context, worldobjects, test)
   local playerObj = getSpecificPlayer(player)
-  
-  if clickedPlayer then
+
+  print(playerObj)
+  print(clickedPlayer)
+  if clickedPlayer and not (clickedPlayer == playerObj) then
     if test == true then
       return true;
     end
 
-    print(clickedPlayer:getDisplayName())
     local option = context:addOption("Share map", worldobjects, MapShare.onShareMap, playerObj, clickedPlayer)
 
     if math.abs(playerObj:getX() - clickedPlayer:getX()) > 2 or math.abs(playerObj:getY() - clickedPlayer:getY()) > 2 then
-			local tooltip = ISWorldObjectContextMenu.addToolTip();
-			option.notAvailable = true;
-			tooltip.description = getText("ContextMenu_GetCloser", clickedPlayer:getDisplayName());
-			option.toolTip = tooltip;
-		end
+      local tooltip = ISWorldObjectContextMenu.addToolTip();
+      option.notAvailable = true;
+      tooltip.description = getText("ContextMenu_GetCloser", clickedPlayer:getDisplayName());
+      option.toolTip = tooltip;
+    end
 
     if not ISWorldMap_instance then
       local tooltip = ISWorldObjectContextMenu.addToolTip();
-			option.notAvailable = true;
-			tooltip.description = "Open your own map at least once first"
-			option.toolTip = tooltip;
+      option.notAvailable = true;
+      tooltip.description = "Open your own map at least once first"
+      option.toolTip = tooltip;
     end
   end
 end
 
+Events.OnReceiveGlobalModData.Add(MapShare.onReceiveGlobalModData)
 Events.OnFillWorldObjectContextMenu.Add(shareMapButton)
 
 
